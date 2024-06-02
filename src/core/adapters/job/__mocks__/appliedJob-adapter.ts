71ae15dca3e29735cd76abfe42ab77c73177fe7c
Sahar Bracha <saharulit@gmail.com>
2024-06-02 13:59:56 +0300

    fix job card entity and mock

--- src/core/adapters/job/__mocks__/appliedJob-adapter.ts
+++ src/core/adapters/job/__mocks__/appliedJob-adapter.ts
@@ -1,24 +1,32 @@
-import { AppliedJob } from '../../../entities/appliedJob';
+import { AppliedJob, JobStatus } from '../../../entities/appliedJob';
 
 export const appliedJobMock: AppliedJob[] = [
   {
+    id: '1',
     title: 'Software Engineer',
     description: 'Develop software solutions',
     company: {
+      id: '1',
+      userId: '1',
       name: 'Google',
       description: 'Search engine',
     },
     applicationLink: 'https://google.com',
     applicationDate: new Date(),
+    status: JobStatus.APPLIED,
   },
   {
-    title: 'Frontend Developer',
+    id: '2',
+    title: 'Frontend Developer Frontend Developer Frontend Developer',
     description: 'Develop frontend solutions',
     company: {
+      id: '2',
+      userId: '1',
       name: 'Facebook',
       description: 'Social media',
     },
     applicationLink: 'https://facebook.com',
     applicationDate: new Date(),
+    status: JobStatus.INTERVIEW,
   },
 ];
--- src/core/entities/appliedJob.ts
+++ src/core/entities/appliedJob.ts
@@ -1,14 +1,24 @@
 export interface AppliedJob {
+  id: string;
   title: string;
-  description: string;
+  description?: string;
   company: Company;
   applicationLink?: string;
   applicationDate?: Date;
-  status: 'applied' | 'interview' | 'rejected' | 'accepted';
+  status: JobStatus;
   comments?: string;
 }
 
 export interface Company {
+  id: string;
+  userId: string;
   name: string;
   description?: string;
+}
+
+export enum JobStatus {
+  APPLIED = 'applied',
+  INTERVIEW = 'interview',
+  REJECTED = 'rejected',
+  ACCEPTED = 'accepted',
 }
