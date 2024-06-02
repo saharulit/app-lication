1f32732fc44bcbb3d497afe26e8de7b611f396ce
Sahar Bracha <saharulit@gmail.com>
2024-06-02 12:38:15 +0300

    create link component

---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,26 @@
+interface LinkProps {
+  mode: 'primary' | 'secondary';
+  size: 'small' | 'medium' | 'large';
+  href: string;
+  target: string;
+  children: React.ReactNode;
+}
+
+const sizes = {
+  small: 'text-sm',
+  medium: 'text-base',
+  large: 'text-lg',
+};
+
+const modes = {
+  primary: 'app-color-deep-dark-blue hover:app-color-blue',
+  secondary: 'app-color-blue hover:app-color-deep-dark-blue',
+};
+
+export default function Link({ mode, size, href, target, children }: LinkProps) {
+  return (
+    <a href={href} target={target} className={`${sizes[size]} ${modes[mode]} underline`}>
+      {children}
+    </a>
+  );
+}
