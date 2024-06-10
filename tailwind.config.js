363604d711afeb3df2d15008de951941d5cfc786
Sahar Bracha <saharulit@gmail.com>
2024-06-10 13:58:18 +0300

    add colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        deepDarkBlue: '#181D31',
+        gray: '#797E93',
+        green: '#34D391',
+        red: '#E8697E',
+        lightPurple: '#E8D9EC',
+        blue: '#5271FF',
+      },
+    },
   },
   plugins: [],
 }
-
