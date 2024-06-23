c4cc613be0fdbb27405582a793ad5151b6c29c86
Sahar Bracha <saharulit@gmail.com>
2024-06-23 14:18:16 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,13 @@
-const a = [];
-a.__proto__.push;
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  test: {
+    environment: 'jsdom',
+    setupFiles: './setupTests.js',
+    globals: true,
+  },
+});
