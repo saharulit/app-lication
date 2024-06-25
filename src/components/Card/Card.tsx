180e7b04ac4411105139eb797bbf3808bf891622
Sahar Bracha <saharulit@gmail.com>
2024-06-25 15:11:33 +0300

    fix card chip colors

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,47 +1,57 @@
-import {
-  Card as MuiCard,
-  CardContent,
-  Avatar,
-  Chip,
-} from '@mui/material';
+import { Card as MuiCard, CardContent, Avatar, Chip } from '@mui/material';
 import { AppliedJob } from '../../core/entities/appliedJob';
 import Link from '../Link/Link';
 
 export default function Card(cardProps: AppliedJob) {
-
   const getChipColor = (status: string) => {
     switch (status) {
       case 'Applied':
-        return 'bg-gray';
+        return 'app-bg-gray';
       case 'Interview':
-        return 'bg-orange';
+        return 'app-bg-orange';
       case 'Rejected':
-        return 'bg-red';
+        return 'app-bg-red';
       case 'Hired':
-        return 'bg-green';
+        return 'app-bg-green';
       default:
-        return 'bg-gray';
+        return 'app-bg-gray';
     }
-  }
+  };
 
   return (
     <MuiCard sx={{ minWidth: 300, minHeight: 200, borderRadius: 8 }}>
       <CardContent>
         <div className="flex flex-col gap-4">
-          <Link mode="primary" size="large" href="#" target="_self" className='max-w-60 line-clamp-2 min-h-14'>
+          <Link
+            mode="primary"
+            size="large"
+            href="#"
+            target="_self"
+            className="max-w-60 line-clamp-2 min-h-14"
+          >
             {cardProps.title}
           </Link>
-          <div className="flex flex-row items-center gap-2 bg-orange">
-            <Avatar alt="company-logo" src={cardProps.company?.logo} sx={{ width: 27, height: 27 }}>
-              <div className="text-xs font-semibold">{cardProps.company?.name?.charAt(0)}</div>
+          <div className="flex flex-row items-center gap-2">
+            <Avatar
+              alt="company-logo"
+              src={cardProps.company?.logo}
+              sx={{ width: 27, height: 27 }}
+            >
+              <div className="text-xs font-semibold">
+                {cardProps.company?.name?.charAt(0)}
+              </div>
             </Avatar>
-            <div className="text-xs font-semibold">{cardProps.company?.name}</div>
+            <div className="text-xs font-semibold">
+              {cardProps.company?.name}
+            </div>
           </div>
           <div className="line-clamp-4 max-w-60 min-h-16">
             <p className="text-xs">{cardProps.company?.description}</p>
           </div>
           <Chip
-            className={`self-end !${getChipColor(cardProps.status)}`}
+            className={`self-end !${getChipColor(
+              cardProps.status
+            )} !app-color-white`}
             label={cardProps.status}
           />
         </div>
--- src/routes/jobs/Jobs.tsx
+++ src/routes/jobs/Jobs.tsx
@@ -1,18 +1,17 @@
 import { SetStateAction, useEffect, useState } from 'react';
 import { AppliedJob } from '../../core/entities/appliedJob';
 import { jobService } from '../../core/services';
-import Card  from '../../components/Card/Card';
+import Card from '../../components/Card/Card';
 import Input from '../../components/Input/Input';
-
 
 export default function Jobs() {
   const [jobs, setJobs] = useState([] as AppliedJob[]);
   const [search, setSearch] = useState('');
 
-  const onSearchChange = (e: { target: { value: SetStateAction<string>; }; }) => {
+  const onSearchChange = (e: { target: { value: SetStateAction<string> } }) => {
     setSearch(e.target.value);
     //TODO: Implement search using url query params
-  }
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -25,15 +24,21 @@ export default function Jobs() {
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-3xl font-bold underline">My applications</h1>
-      <Input name="search" type="text" placeholder="Search" onChange={onSearchChange}/>
-      { jobs.length > 0 ?
-        <div className='flex flex-wrap gap-4'>
+      <Input
+        name="search"
+        type="text"
+        placeholder="Search"
+        onChange={onSearchChange}
+      />
+      {jobs.length > 0 ? (
+        <div className="flex flex-wrap gap-4">
           {jobs.map((job, index) => (
-            <Card key={index} {...job}/>
+            <Card key={`card-${index + 1}`} {...job} />
           ))}
-        </div> : 
+        </div>
+      ) : (
         <p className="text-lg">No jobs found</p>
-      }
+      )}
     </div>
   );
 }
