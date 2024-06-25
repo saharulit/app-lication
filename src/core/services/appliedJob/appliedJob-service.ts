31b9ece0e96b4f4324b5c09546cb67bb432d240f
saharulit <saharulit@gmail.com>
2024-06-25 10:57:31 +0000

    Deploying to gh-pages from  @ 242ee10951d2d8c2364bed4222f889d59f3f1605 🚀

--- src/core/services/appliedJob/appliedJob-service.ts
+++
@@ -1,8 +0,0 @@
-import { AppliedJobAdapter } from '../../adapters/job/appliedJob-adapter';
-
-export class AppliedJobService {
-  constructor(private adapter: AppliedJobAdapter) {}
-  appliedJobList(search?: string) {
-    return this.adapter.appliedJobList(search);
-  }
-}
--- src/core/services/index.ts
+++
@@ -1,6 +0,0 @@
-import { AppliedJobAdapter } from '../adapters/job/appliedJob-adapter';
-import { AppliedJobService } from './appliedJob/appliedJob-service';
-
-const jobAdapter = new AppliedJobAdapter();
-
-export const jobService = new AppliedJobService(jobAdapter);
