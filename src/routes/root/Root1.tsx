c23bd4efacd4fc0f6407ec4ac99776669a286fcb
Sahar Bracha <saharulit@gmail.com>
2024-06-27 15:03:24 +0300

    test: check current path

--- src/routes/root/Root1.tsx
+++ src/routes/root/Root1.tsx
@@ -1,10 +1,13 @@
 import MainLayout from '../+layout/MainLayout1';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Jobs from '../jobs/Jobs';
 
 const Root = () => {
+  const location = useLocation();
+  const { pathname } = location;
   return (
     <MainLayout>
+      <div>pathName- {pathname}</div>
       <Routes>
         <Route path="/" element={<Navigate to="/jobs" replace />} />
         <Route path="/jobs" element={<Jobs />} />
