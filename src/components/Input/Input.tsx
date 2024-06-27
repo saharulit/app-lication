e672a700339dd2f78180c4f1127ca0d2de183e84
Sahar Bracha <saharulit@gmail.com>
2024-06-27 09:56:09 +0300

    update input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  placeholder: string;
-  type: string;
+  placeholder?: string;
+  type?: string;
+  label?: string;
 }
 
-const Input: React.FC<InputProps> = ({ placeholder, type, ...rest }) => {
+const Input: React.FC<InputProps> = ({
+  placeholder,
+  type = 'text',
+  label,
+  ...rest
+}) => {
   return (
-    <input
-      type={type}
-      placeholder={placeholder}
-      className="app-bg-white rounded-lg p-4"
-      {...rest}
-    />
+    <div className="flex flex-col">
+      <label className="font-semibold">{label}</label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        className="app-bg-white rounded-md p-3 border app-bg-border"
+        {...rest}
+      />
+    </div>
   );
 };
 
