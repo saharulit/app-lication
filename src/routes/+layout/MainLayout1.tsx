ac4af305d060b047e5e9735aee8313de85bd2ffd
Sahar Bracha <saharulit@gmail.com>
2024-06-27 15:26:55 +0300

    test: check current path

--- src/routes/+layout/MainLayout1.tsx
+++ src/routes/+layout/MainLayout1.tsx
@@ -1,6 +1,7 @@
 import { useCookies } from 'react-cookie';
 import { ReactNode } from 'react';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import { useLocation } from 'react-router-dom';
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -11,10 +12,14 @@ function MainLayout({ children }: MainLayoutProps): JSX.Element {
   const handleLogOut = () => {
     setCookie('user', false, { path: '/' });
   };
+  const location = useLocation();
+  const { pathname } = location;
 
   return (
     <>
       <NavigationBar onLogOut={handleLogOut} />
+      <div>pathName- {pathname}</div>
+
       <div className="p-10">{children}</div>
     </>
   );
