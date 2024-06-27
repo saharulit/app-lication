3b3f08fb2cdc99914b980c391e65de04deabb5b2
Sahar Bracha <saharulit@gmail.com>
2024-06-27 14:15:55 +0300

    revert navigation bar

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -2,14 +2,14 @@ import { Button } from '@mui/material';
 import Logo from '../../assets/logo-full-white.svg';
 
 interface NavigationBarProps {
-  onLogOut: () => void;
+  onLogOut: () => void; // Specify the type for onLogOut prop
 }
 
 function NavigationBar({ onLogOut }: NavigationBarProps) {
   return (
     <div className="w-full h-16 app-bg-deep-dark-blue">
-      <div className="flex flex-row items-center justify-between px-10">
-        <img src={Logo} alt="logo" className="h-8" />
+      <div className="flex flex-row">
+        <img className="pl-10" src={Logo} alt="logo" />
         <Button onClick={onLogOut}>Logout</Button>
       </div>
     </div>
