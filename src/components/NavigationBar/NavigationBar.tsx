794d3dac789eebd88a8a10fbbf0ceaede005b6d3
Sahar Bracha <saharulit@gmail.com>
2024-07-07 17:27:10 +0300

    fix navigation bar

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -7,17 +7,15 @@ interface NavigationBarProps {
 
 function NavigationBar({ onLogOut }: NavigationBarProps) {
   return (
-    <>
-      <div className="w-full h-16 app-bg-deep-dark-blue">
-        <div className="flex flex-row px-10 items-center">
-          <img src={Logo} alt="logo" />
-          <div className="grow" />
-          <Button className="custom-button" onClick={onLogOut}>
-            Logout
-          </Button>
-        </div>
+    <div className="w-full h-16 app-bg-deep-dark-blue">
+      <div className="flex flex-row px-10 items-center">
+        <img src={Logo} alt="logo" />
+        <div className="grow" />
+        <Button className="custom-button" onClick={onLogOut}>
+          Logout
+        </Button>
       </div>
-    </>
+    </div>
   );
 }
 
