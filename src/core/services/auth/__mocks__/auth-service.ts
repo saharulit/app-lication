88b5adf145d71cfed6d37e3d18fc71969a41287a
Sahar Bracha <saharulit@gmail.com>
2024-07-07 15:47:22 +0300

    add tests with comments

---
+++ src/core/services/auth/__mocks__/auth-service.ts
@@ -0,0 +1,13 @@
+// import { mockUser } from 'src/core/adapters/auth/__mocks__/auth-adapter';
+
+// export const mockLogin = vi.fn().mockImplementation(async () => {
+//   return { success: true, user: mockUser };
+// });
+// export const mockLogout = vi.fn().mockImplementation(async () => {
+//   return { success: true };
+// });
+
+// export const AuthService = vi.fn().mockImplementation(() => ({
+//   login: mockLogin,
+//   logout: mockLogout,
+// }));
