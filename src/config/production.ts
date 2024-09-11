4ac9d1f918928f48c2c3045071ceb15810235fee
Sahar Bracha <saharulit@gmail.com>
2024-09-11 12:35:41 +0300

    change BASENAME for vercel

--- src/config/production.ts
+++ src/config/production.ts
@@ -2,6 +2,6 @@ import type { AppConfig } from './types';
 
 export const production: AppConfig = {
   ENV: 'production',
-  BASENAME: '/app-lication',
+  BASENAME: '/',
   SERVER_BASE_URL: 'https://app-lication-server.vercel.app/api/',
 };
