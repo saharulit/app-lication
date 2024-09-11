cc67d5e45fd1a31952c868f243e2960c26d77cb0
Sahar Bracha <saharulit@gmail.com>
2024-09-11 12:34:26 +0300

    test

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { config } from './config/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <div>TEST Sahar</div>
     <BrowserRouter basename={config.BASENAME}>
       <App />
     </BrowserRouter>
