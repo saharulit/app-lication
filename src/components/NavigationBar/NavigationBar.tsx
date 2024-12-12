e722aed51476f1420f59105d08d7a50a6b47c695
Sahar Bracha <saharulit@gmail.com>
2024-12-12 14:54:13 +0200

    fix logo

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -8,8 +8,8 @@ interface NavigationBarProps {
 function NavigationBar({ onLogOut }: NavigationBarProps) {
   return (
     <div className="w-full h-16 app-bg-deep-dark-blue">
-      <div className="flex flex-row px-10 items-center">
-        <img src={Logo} alt="logo" />
+      <div className="flex flex-row px-10 py-3 items-center">
+        <img src={Logo} alt="logo" className="w-24" />
         <div className="grow" />
         <Button className="custom-button" onClick={onLogOut}>
           Logout
